003c149ce1bd83f81566086f0a8e71ae921cba00
kuntack.lee <kuntack.lee@gmail.com>
2026-03-16 09:17:37 +0000

    이력 저장

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-2ae722a1'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "/index.html",
-    "revision": "0.rb715f0l3oo"
+    "revision": "0.84s655o9hrg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/index.html"), {
